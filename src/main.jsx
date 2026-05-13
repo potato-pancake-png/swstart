2b4a03aeeaa6caf7349dbe53e4d163f78f270260
potato-pancake-png <yurizoa12369@gmail.com>
2026-05-13 17:07:06 +0900

    Inject GA4 via main.jsx to bypass Vite 8 script tag removal

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,16 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
+const GA_ID = 'G-BB8YRNS9DT'
+const s = document.createElement('script')
+s.async = true
+s.src = `https://www.googletagmanager.com/gtag/js?id=${GA_ID}`
+document.head.appendChild(s)
+window.dataLayer = window.dataLayer || []
+window.gtag = function () { window.dataLayer.push(arguments) }
+window.gtag('js', new Date())
+window.gtag('config', GA_ID)
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
